bd3e34de9104a27f9d4cacb68f4ec4533d2effb0
yunkooo <yunkoooooo@gmail.com>
2022-07-29 23:12:30 +0900

    BAEKJOON 꾸준히

---
+++ BAEKJOON/1차원 배열/B3052/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+
+let input = fs.readFileSync(filePath,'utf-8').split('\n').map(a => Number(a));
+input = input.map(t => t%42);
+// let result = input.filter((n, idx) => input.indexOf(n) === idx);
+// console.log(result.length);
+
+let result =[];
+input.forEach(val => {
+    if(result.indexOf(val) === -1) result.push(val);
+})
+
+console.log(result.length);
+
+//두가지 방법을 이용해도 fail
