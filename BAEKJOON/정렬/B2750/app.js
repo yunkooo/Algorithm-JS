2228990a6868ed04681190f87375d2b7554d61c1
yunkooo <yunkoooooo@gmail.com>
2022-08-08 18:22:00 +0900

    B2750 풀이 수정

--- BAEKJOON/정렬/B2750/app.js
+++ BAEKJOON/정렬/B2750/app.js
@@ -3,5 +3,9 @@ const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 
 let input = fs.readFileSync(filePath).toString().split('\n');
 let n = input.shift();
-input.map(val => Number(val)).sort((a,b) => a-b);
+input = input.map(val => Number(val)).sort((a,b) => {
+    if(a>b) return 1;
+    if(a<b) return -1;
+    if(a===b) return 0;
+});
 input.forEach(val => console.log(val));
