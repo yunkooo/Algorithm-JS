eb279637f2aa553329eee7f81b42555af3440235
yunkooo <yunkoooooo@gmail.com>
2023-01-19 00:03:41 +0900

    프로그래머스 lv0 수정

--- programmers/LV_0/옷가게 할인 받기.js
+++ programmers/LV_0/옷가게 할인 받기.js
@@ -1,6 +1,6 @@
 function solution(price) {
   if (price >= 500000) return Math.floor(price * 0.8);
-  else if (price >= 3000000) return Math.floor(price * 0.9);
-  else if (price >= 100000) return Math.floor(price * 0.95);
+  else if (price >= 300000 && price < 3000000) return Math.floor(price * 0.9);
+  else if (price >= 100000 && price < 1000000) return Math.floor(price * 0.95);
   else return price;
 }
