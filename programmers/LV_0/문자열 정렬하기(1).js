f51d581a0ccfad8730883a7a6b8ae443cebe391c
yunkooo <yunkoooooo@gmail.com>
2023-02-06 00:01:53 +0900

    프로그래머스 lv 0 수정

--- programmers/LV_0/문자열 정렬하기(1).js
+++ programmers/LV_0/문자열 정렬하기(1).js
@@ -1,4 +1,8 @@
 function solution(my_string) {
-  var answer = my_string.split("").map((val) => Number(val));
+  var answer = my_string
+    .split("")
+    .map((val) => Number(val))
+    .filter((val) => !isNaN(val))
+    .sort((a, b) => a - b);
   return answer;
 }
